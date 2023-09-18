f8ad4d15d58eb683d7623cb19c1c822035de810a
sarah <bennes7@mail.us.edu>
2023-09-18 15:44:52 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int purchasePrice = 25;
+        double salesTax = purchasePrice * .05;
+        System.out.println("The sales tax on a purchase of " + " $ " + purchasePrice + " is " + salesTax);
+    }
+}
